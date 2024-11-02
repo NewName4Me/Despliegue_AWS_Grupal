90604ef9dcb1f42810594c0ba0830375bd163a78
NewName4ME <tortarod@gmail.com>
2024-11-02 18:30:16 +0100

    actualizar srcript ejecucaion

--- command.sh
+++ command.sh
@@ -3,9 +3,9 @@ sudo yum update -y
 sudo yum install -y httpd git
 sudo mkdir -p /var/www/html
 # Clona el repositorio de GitHub
-sudo git clone https://github.com/NewName4Me/Chess-Web-For-Class.git /tmp/tu_repositorio
+sudo git clone https://github.com/NewName4Me/Despliegue_AWS_Grupal.git /tmp/tu_repositorio
 # Mueve solo la carpeta deseada al directorio web
-sudo mv /tmp/tu_repositorio/ProyectoFinal2Trimestre/* /var/www/html/
+sudo mv /tmp/tu_repositorio/web/* /var/www/html/
 # Limpia eliminando el repositorio clonado
 sudo rm -rf /tmp/tu_repositorio
 sudo systemctl enable httpd
