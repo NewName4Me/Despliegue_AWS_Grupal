69a9ff1afcb90812ac43c0238822fc69de31310d
NewName4ME <tortarod@gmail.com>
2024-11-10 18:58:39 +0100

    add random funciont

--- web/js/hero.js
+++ web/js/hero.js
@@ -25,3 +25,22 @@ function addYearToFooter() {
 function iniciarApp() {
     addYearToFooter();
 }
+
+
+/**
+ * Funcion que hace un console.log con un mensaje de prueba
+ * 
+ * @function testWorkflow
+ * @param {String} message 
+ * @param {Boolean} throwError 
+ * @throws {Error} 
+ * @returns {String} 
+ */
+export function testWorkflow(message, throwError) {
+    if (throwError) {
+        throw new Error('Este es un error de prueba');
+    }
+    console.log(message);
+    return 'La prueba termino exitosamente';
+}
+
