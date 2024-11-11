1b4338d365c4b0d449b105166457f66abda69d99
NewName4ME <tortarod@gmail.com>
2024-11-11 19:38:20 +0100

    por fin funciona en php

--- command.sh
+++ command.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 sudo yum update -y
-sudo yum install -y httpd git
+sudo yum install -y httpd httpd-tools mod_ssl
+sudo amazon-linux-extras enable php8.2
+sudo yum clean metadata
+sudo yum install git php php-php-common php-pear -y
 sudo mkdir -p /var/www/html
 # Clona el repositorio de GitHub
 sudo git clone https://github.com/NewName4Me/Despliegue_AWS_Grupal.git /tmp/tu_repositorio
@@ -8,5 +11,8 @@ sudo git clone https://github.com/NewName4Me/Despliegue_AWS_Grupal.git /tmp/tu_r
 sudo mv /tmp/tu_repositorio/web/* /var/www/html/
 # Limpia eliminando el repositorio clonado
 sudo rm -rf /tmp/tu_repositorio
+#corrigiendo permisos por fis
+sudo chown -R apache:apache /var/www/html
+sudo chmod -R 755 /var/www/html
 sudo systemctl enable httpd
 sudo systemctl start httpd
--- web/js/utils/getYear.js
+++
@@ -1,7 +0,0 @@
-/**
- * Una función que nos devuelve el año actual
- * @returns {Date} 
- */
-export function getCurrentYear() {
-    return new Date().getFullYear();
-}
